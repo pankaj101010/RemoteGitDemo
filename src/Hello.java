a53aba11413bcc258b90f4ff573548dc5a097c8d
PANKAJ <pankajdevgade10@gmail.com>
2019-03-23 15:09:11 +0530

    new old change

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,6 @@ public class Hello {
 		System.out.println("changes by devloper 2");
 		// changes by old branch
 		System.out.println("old branch commit");
-		// old changed again
 		System.out.println("my new old chnaged");
 	}
 
